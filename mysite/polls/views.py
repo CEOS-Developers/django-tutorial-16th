ac8cd8aa5973e7fda87454271c991ef9f4e6605d
bokdol11859 <2019147551@yonsei.ac.kr>
2022-09-20 17:58:05 +0900

    refactor: href format fixed in templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.template import loader
@@ -16,7 +16,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = f"You're looking at the results of question {question_id}."
