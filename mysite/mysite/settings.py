ec8d4e69799536b2f51ed6942283b994c2ae63e7
bokdol11859 <2019147551@yonsei.ac.kr>
2022-09-20 18:01:45 +0900

    refactor: secret key separeted into env file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import environ
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w+!3paz4vp9qv5#(mqb!yp0enh^k#v+)n!jz644&&5233%#s=u'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
