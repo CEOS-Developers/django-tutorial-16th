039321c7a83ad1cfaed3363690e20e64317d10ec
squirMM <jsm6616@naver.com>
2022-09-22 00:37:29 +0900

    generic

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,12 +6,8 @@ from . import views
 app_name = 'polls'
 # name : url 의 이름 , 어디에서나 명확하게 참조가능하도록
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
