750bbb5f2512827f9afaabab13ec2a8234708975
suhhyun <2835223@ewhain.net>
2022-09-22 00:41:31 +0900

    Add index page
    create and connect index template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
+from django.template import loader
 
 from .models import Question
 
@@ -6,8 +7,12 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
