81c6b6364c31ff796db5a392c63a11a8e5815da2
geniee44 <68368633+geniee44@users.noreply.github.com>
2022-09-22 20:19:49 +0900

    Refactor polls url path

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
