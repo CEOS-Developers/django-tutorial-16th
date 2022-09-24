24d4a1b421cceb25e1871deab550bc306ce601ca
geniee44 <68368633+geniee44@users.noreply.github.com>
2022-09-24 20:44:00 +0900

    Upgrade 404 error by using shortcuts

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 # from django.template import loader
 
 from .models import Question
@@ -25,11 +25,15 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+    # legacy code (basic 404 error handler)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
