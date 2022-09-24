258c221934488c0a97bd192643d5030a24d09db3
geniee44 <68368633+geniee44@users.noreply.github.com>
2022-09-24 20:30:36 +0900

    Upgrade index view by using templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
 
+# polls/index.html 템플릿을 불러와 context를 전달
+# context는 템플릿에서 쓰이는 변수명과 Python 객체를 연결
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
